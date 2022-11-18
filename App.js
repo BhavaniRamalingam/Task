182d65ea7ac6c36224e22930a3a69cf3ea4b3d71
BhavaniRamalingam <bhavani.priya397@gmail.com>
2022-11-18 17:55:12 +0530

    Squares

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component<{}> {
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <View style={[styles.SquareShapeView,{backgroundColor:"skyblue"}]}>
+          <Text style={[styles.Value]}>Square1</Text>
+        </View>
+        <View style={[styles.SquareShapeView,{backgroundColor:"#66b3ff"}]} >
+          <Text style={styles.Value}>Square2</Text>
+        </View>
+        <View style={[styles.SquareShapeView,{backgroundColor:"#ff4da6"}]}>
+          <Text style={styles.Value}>Square3</Text>
+        </View>  
+
+      </View>
+    );
+  }
 }
 
+
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    display:'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    marginTop:10,
+    marginLeft:10,
+    paddingRight:10,
+    flexDirection:'row',
+    border:1,
+    borderColor:'black'
+    
   },
+
+  SquareShapeView: {
+
+    width: 100,
+    height: 100,
+    paddingBottom:5,
+    margin:12,
+    value:'Square1',
+    marginBottom:5,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  Value:{
+    color:'black',
+  }
+
 });
